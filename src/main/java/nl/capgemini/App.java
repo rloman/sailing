042fe09e76bc9b07fc2937110b3f581ccc1a676a
Raymond Loman <raymond@carpago.nl>
2017-11-10 09:58:44 +0100

    Add first excpetion

--- src/main/java/nl/capgemini/App.java
+++ src/main/java/nl/capgemini/App.java
@@ -1,13 +1,26 @@
 package nl.capgemini;
 
+import nl.capgemini.math.Wiskunde;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+
+        int quotient = Wiskunde.divide(8,4);
+
+        System.out.println("Het quotient van ... is "+quotient);
+
+        quotient = Wiskunde.divide(15,3);
+
+        System.out.println(quotient);
+
+        quotient = Wiskunde.divide(4,0);
+
+        System.out.println(quotient);
+
+
+
     }
 }
---
+++ src/main/java/nl/capgemini/math/Wiskunde.java
@@ -0,0 +1,16 @@
+package nl.capgemini.math;
+
+public final class Wiskunde {
+
+    public static int divide(int teller, int noemer) {
+
+        int result = teller/noemer;
+
+        return result;
+    }
+
+
+    private Wiskunde() {
+
+    }
+}
